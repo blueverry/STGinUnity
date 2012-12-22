c998eb958eb59038c823f2fa343fd571963dec92
blueverry <monyaxeno@gmail.com>
2012-12-22 11:28:54 +0900

    敵の種類増加

--- Assets/EnemyAppearance.js
+++ Assets/EnemyAppearance.js
@@ -3,16 +3,18 @@ var enemyPrefab : GameObject;
 var enemyTime = 1.0;
 var enemyType : String[];
 
-private function EnemyAppear (appearancePosition : Vector3, letter : String)
+private function EnemyAppear (appearancePosition : Vector3, letter : String, enemySpeed : float, enemyLife : int, 
+							  enemyColor : String, enemyScale : float)
 {
-	var enemyLife = 30;
 
 	for (var c in letter)
 	{
 		var enemy = Instantiate(enemyPrefab, appearancePosition, transform.rotation);
-		appearancePosition += Vector3(0, 0, 1.5);
-		enemy.GetComponentInChildren(TextMesh).text = "" + c;
+		appearancePosition += Vector3(0, 0, 1.5 * enemyScale);
+		enemy.GetComponentInChildren(TextMesh).text = "<color="+enemyColor+">" + c + "</color>";
 		enemy.GetComponent(EnemyControl).enemyLife = enemyLife;
+		enemy.GetComponent(EnemyControl).enemySpeed = enemySpeed;
+		enemy.transform.localScale = enemyScale * Vector3(1, 1, 1);
 		enemyLife = Mathf.Max(enemyLife - 5, 5);
 	}
 }
@@ -20,9 +22,32 @@ private function EnemyAppear (appearancePosition : Vector3, letter : String)
 function Start () {
 	while(true)
 	{
-		var appearancePositionRandomZ = Random.Range(-7.0, 7.0);
-		EnemyAppear( transform.position + Vector3(appearancePositionRandomZ ,0 ,0),
-					 enemyType[Random.Range(0, enemyType.Length)]);
+		var randomValue = Random.value;
+		
+		if (randomValue <= 0.4)
+		{
+			var appearancePositionRandomX = Random.Range(-7.0, 7.0);
+			EnemyAppear( transform.position + Vector3(appearancePositionRandomX ,0 ,0),
+						 enemyType[Random.Range(0, enemyType.Length)], 0.05, 30, "white", 1);
+		}
+		else if (randomValue > 0.4 && randomValue <= 0.8)
+		{
+			var offset = Random.Range(-2.0, 2.0);
+			var space = Random.Range(2.0, 4.0);
+			for (var i= 0; i < 7; i++)
+			{
+				appearancePositionRandomX = Random.Range(-7.0, -6.0);
+				EnemyAppear( transform.position + Vector3(i * space - 7.0 + offset,0 ,i),
+							 "―＝十", 0.2, 20, "#80ff80", 1);
+			}
+		}
+		else
+		{
+			appearancePositionRandomX = Random.Range(-7.0, 7.0);
+			EnemyAppear( transform.position + Vector3(appearancePositionRandomX ,0 ,0),
+						 enemyType[Random.Range(0, enemyType.Length)], 0.03, 60, "red", 2);
+		
+		}
 		yield WaitForSeconds(enemyTime);
 	}
 }
--- Assets/EnemyControl.js
+++ Assets/EnemyControl.js
@@ -3,25 +3,12 @@ var enemySpeed = 0.05;
 var enemyBulletPrefab : GameObject;
 var bulletTime = 0.1;
 var enemyLife = 30;
-// var enemyType = { A, B, C };
 
 function Start ()
 {
 	while(true)
 	{
-		// if (enemyType == A)
-		// {
-		// Instantiate(enemyBulletPrefab, transform.position, transform.rotation * Quaternion.AngleAxis(10, Vector3(0, 1, 0)));
-		// Instantiate(enemyBulletPrefab, transform.position, transform.rotation * Quaternion.AngleAxis(-10, Vector3(0, 1, 0)));
 		yield WaitForSeconds(bulletTime);
-		// }
-		// if else (enemyType == B)
-		// {
-		//		Instantiate(enemyBulletPrefab, transform.position, transform.rotation * Quaternion.AngleAxis(10, Vector3(0, 1, 0)));
-		//		Instantiate(enemyBulletPrefab, transform.position, transform.rotation * Quaternion.AngleAxis(-10, Vector3(0, 1, 0)));
-		//		
-		//		yield WaitForSeconds(bulletTime);
-		// }
 	}
 	
 	yield WaitForSeconds(12.0);
@@ -30,15 +17,7 @@ function Start ()
 
 function Update ()
 {
-	// if (enemyType == A)
-	// {
 	transform.position += transform.forward * enemySpeed;
-	// }
-	// else if (enemyType == B)
-	// {
-	// 		transform.position += transform.forward * enemySpeed;
-	//		transform.rotation += 0.02f;
-	// }
 }
 
 function OnTriggerEnter (other : Collider)
